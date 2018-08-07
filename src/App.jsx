957e0238ace398a38fa1cad3e130cb322e042b42
Rex Chiu <rex.chiu@outlook.com>
2018-08-07 11:01:42 -0400

    passed through messages and currentUser from App to child components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
 import React, { Component, Fragment } from 'react';
 
 import ChatBar from './ChatBar.jsx';
-import Messages from './Messages.jsx';
+import MessageList from './MessageList.jsx';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentUser: { name: 'Cats' }, // optional. if currentUser is not defined, it means the user is Anonymous
+      messages: [
+        {
+          username: 'Cats',
+          content: 'Has anyone seen my marbles?'
+        },
+        {
+          username: 'Anonymous',
+          content: 'No, I think you lost them. You lost your marbles Cats. You lost them for good.'
+        }
+      ]
+    };
+  }
+
   render() {
     return (
       <Fragment>
@@ -12,10 +29,11 @@ class App extends Component {
             Chatty
           </a>
         </nav>
-        <Messages />
-        <ChatBar />
+        <MessageList />
+        <ChatBar currentUser={this.state.currentUser} />
       </Fragment>
     );
   }
 }
+
 export default App;
--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 
 class Messages extends Component {
   render() {
+    const currentUser = this.props.currentUser;
+    const currentName = currentUser.name;
     return (
       <footer className="chatbar">
-        <input className="chatbar-username" placeholder="Your Name (Optional)" />
+        <input className="chatbar-username" defaultValue={currentName} placeholder="Your Name (Optional)" />
         <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
       </footer>
     );
   }
 }
+
 export default Messages;
