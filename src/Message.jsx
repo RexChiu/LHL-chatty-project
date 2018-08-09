11304184e1675b8272d49dc3a32df3746c32d8c3
Rex Chiu <rex.chiu@outlook.com>
2018-08-09 12:28:47 -0400

    added functionality to add colors to each user

--- src/Message.jsx
+++ src/Message.jsx
@@ -3,10 +3,15 @@ import React, { Component } from 'react';
 class Message extends Component {
   render() {
     let username = this.props.message.username ? this.props.message.username : 'AnonyCats';
+    let inlineStyle = {
+      color: this.props.message.color
+    };
 
     return (
       <div className="message">
-        <span className="message-username">{username}</span>
+        <span className="message-username" style={inlineStyle}>
+          {username}
+        </span>
         <span className="message-content">{this.props.message.content}</span>
       </div>
     );
